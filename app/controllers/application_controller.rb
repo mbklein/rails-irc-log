5e3d9b64a1b6d740a0f1f41b38de0eae2a618507
Michael Klein <mbklein@gmail.com>
2012-02-29 15:51:54 -0800

    Ensure messages are always pulled with Time.zone set to UTC

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :set_timezone
   
+  def with_time_zone(tz)
+    result = nil
+    old_tz = Time.zone
+    Time.zone = tz
+    begin
+      result = yield
+    ensure
+      Time.zone = old_tz
+    end
+    return result
+  end
+  
   def set_timezone
     if params[:tz]
       cookies[:tz] = { :value => params[:tz], :path => '/', :expires => 10.years.from_now }
